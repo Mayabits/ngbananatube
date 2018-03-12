f5d26f5c63f0ddf9b67b550f8328c10ac0ecacfd
Noemi <noemi.perlopez@gmail.com>
2018-03-12 17:54:37 +0100

    COMIENZO ANGULAR

--- banana/src/app/app.module.ts
+++ banana/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { InsectionComponent } from './insection/insection.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InsectionComponent,
   ],
   imports: [
     BrowserModule
