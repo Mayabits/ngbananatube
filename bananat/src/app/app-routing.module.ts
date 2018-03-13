400d7d6e6979b15f03045936423f3f92428aff75
Noemi <noemi.perlopez@gmail.com>
2018-03-13 17:50:42 +0100

    creadas rutas

--- bananat/src/app/app-routing.module.ts
+++ bananat/src/app/app-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {InsectionComponent} from './insection/insection.component';
+import {ContactoComponent} from './contacto/contacto.component';
+import {QuienessomosComponent} from './quienessomos/quienessomos.component';
+import {TerminosComponent} from './terminos/terminos.component';
+import {PrivacidadComponent} from './privacidad/privacidad.component';
 
 const routes: Routes = [
     {
         path:'',
         component: InsectionComponent
     },
+    {
+        path: 'contacto',
+        component: ContactoComponent
+    },
+    {
+        path: 'quienessomos',
+        component: QuienessomosComponent
+    },
+    {
+        path: 'terminos',
+        component: TerminosComponent
+    },
+    {
+        path: 'privacidad',
+        component: PrivacidadComponent
+    }
 ];
 
 @NgModule({
--- bananat/src/app/app.module.ts
+++ bananat/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { InsectionComponent } from './insection/insection.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { QuienessomosComponent } from './quienessomos/quienessomos.component';
+import { TerminosComponent } from './terminos/terminos.component';
+import { PrivacidadComponent } from './privacidad/privacidad.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InsectionComponent
+    InsectionComponent,
+    ContactoComponent,
+    QuienessomosComponent,
+    TerminosComponent,
+    PrivacidadComponent
   ],
   imports: [
     BrowserModule,
