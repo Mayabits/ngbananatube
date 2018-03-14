7bdd37f9edcdc7de1482fd31edabaa2ca859572a
Noemi <noemi.perlopez@gmail.com>
2018-03-14 17:00:09 +0100

    mostrar-ocultar header

--- bananat/src/app/app.component.ts
+++ bananat/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements {
+    public location = '';
+    constructor(private _router:Router){
+        this.location=_router.url;
+    }
 }
