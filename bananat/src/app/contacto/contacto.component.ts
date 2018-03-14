be712afe2c33a2f42c8c535fc719630a7097bde9
Laura Bermejo <34232594+laurabermejo@users.noreply.github.com>
2018-03-14 13:51:07 +0100

    instalado jquery

--- bananat/src/app/contacto/contacto.component.ts
+++ bananat/src/app/contacto/contacto.component.ts
@@ -8,27 +8,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
-    contacto:Contacto;
-    submitted = false;
-    data:any;
-    url:string;
-    constructor(private http: HttpClient) { 
-        this.url="http://localhost:3000/api/";
-        this.contacto=new Contacto("","","",new Date());
+    constructor() {  
+    
+  }
         
-    }
+    
   ngOnInit() {
   }
   onSubmit() {
-    this.submitted = true;
-    this.contacto.fecha= new Date();
-    console.log(this.contacto);
-      
-    var promesa=this.http.post(this.url,this.contacto);
-        promesa.subscribe(
-            data => this.data = data
-        );
-    
-    
+
   }
 }
