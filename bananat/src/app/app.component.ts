eff5fd5f244e20ea686c158a9a52f7398975ce1b
Laura Bermejo <34232594+laurabermejo@users.noreply.github.com>
2018-03-16 00:57:02 +0100

    intento header

--- bananat/src/app/app.component.ts
+++ bananat/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+public location = '' ;
+
+constructor(private  routes: Router) 
+{      
+  this.location = routes.url;
+}
 }
