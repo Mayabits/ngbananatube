5ca39e3715a53794352f8ae30d324b01a1d78cd6
Laura Bermejo <34232594+laurabermejo@users.noreply.github.com>
2018-03-15 18:14:09 +0100

    contacto validado

--- bananat/src/app/app.module.ts
+++ bananat/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { InsectionComponent } from './insection/insection.component';
@@ -29,7 +30,8 @@ import { RecuperapassComponent } from './recuperapass/recuperapass.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- bananat/src/app/contacto/contacto.component.ts
+++ bananat/src/app/contacto/contacto.component.ts
@@ -8,15 +8,26 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
-    
-    constructor() {  
-    
-  }
-        
+    contacto:Contacto;
+    submitted = false;
+    data:any;
+    url:string;
+    constructor(private http: HttpClient) { 
+        this.url="http://localhost:3000/api/";
+        this.contacto=new Contacto("","","",new Date());
+    }
     
   ngOnInit() {
+      
   }
   onSubmit() {
-
+      this.submitted = true;
+    this.contacto.fecha= new Date();
+    console.log(this.contacto);
+      
+    var promesa=this.http.post(this.url,this.contacto);
+    promesa.subscribe(
+            data => this.data = data
+        );
   }
 }
